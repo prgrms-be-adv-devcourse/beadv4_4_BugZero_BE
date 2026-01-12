487741d7160665ec6cc5ffcd295d115563b381cf
[Kkimdoyeon] <ehdus5176@gmail.com>
2026-01-12 18:07:18 +0900

    chore: 초기 Spring Boot 프로젝트 설정 추가

---
+++ src/main/java/com/bugzero/RareGo/RareGoApplication.java
@@ -0,0 +1,13 @@
+package com.bugzero.RareGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RareGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RareGoApplication.class, args);
+	}
+
+}
