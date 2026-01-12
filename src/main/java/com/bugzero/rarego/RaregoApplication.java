cf1447c5fb9c1ae46977f34a37e4ce5b4a4c90c7
gumguard <gawonlee0712@gmail.com>
2026-01-12 20:42:29 +0900

    chore(init): springboot 초기설정

---
+++ src/main/java/com/bugzero/rarego/RaregoApplication.java
@@ -0,0 +1,13 @@
+package com.bugzero.rarego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaregoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaregoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bugzero/rarego/RaregoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugzero.rarego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaregoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
