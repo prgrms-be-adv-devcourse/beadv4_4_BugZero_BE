e9190597f7bb57f33d52cbdeebe10c53a14bc442
jin214930 <jin214930@naver.com>
2026-01-13 11:53:08 +0900

    refactor(response): 인터페이스 도입

--- src/main/java/com/bugzero/rarego/global/exception/ExceptionResponseDto.java
+++ src/main/java/com/bugzero/rarego/global/exception/ExceptionResponseDto.java
@@ -1,6 +1,8 @@
 package com.bugzero.rarego.global.exception;
 
-public record ExceptionResponseDto(Integer status, String message) {
+import com.bugzero.rarego.standard.response.ResponseDto;
+
+public record ExceptionResponseDto(Integer status, String message) implements ResponseDto {
 
 	public static ExceptionResponseDto to(Integer status, String message) {
 		return new ExceptionResponseDto(status, message);
---
+++ src/main/java/com/bugzero/rarego/standard/response/ResponseDto.java
@@ -0,0 +1,7 @@
+package com.bugzero.rarego.standard.response;
+
+public interface ResponseDto {
+	Integer status();
+
+	String message();
+}
