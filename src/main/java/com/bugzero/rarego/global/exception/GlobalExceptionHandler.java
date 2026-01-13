fb5805b7b543c02540acfabc476df6e30bbc6abc
jin214930 <jin214930@naver.com>
2026-01-13 11:56:55 +0900

    refactor(response): 예외처리기에서 상태코드 세팅 없이 dto만 반환하도록 수정

--- src/main/java/com/bugzero/rarego/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bugzero/rarego/global/exception/GlobalExceptionHandler.java
@@ -1,20 +1,18 @@
 package com.bugzero.rarego.global.exception;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(CustomException.class)
-	public ResponseEntity<ExceptionResponseDto> handleCustomException(CustomException e) {
+	public ExceptionResponseDto handleCustomException(CustomException e) {
 		log.error("CustomException 발생: {}", e.getMessage());
-		return ResponseEntity.status(e.getErrorType().getHttpStatus())
-			.body(ExceptionResponseDto.to(e.getErrorType().getHttpStatus(), e.getErrorType().getMessage()));
+		return ExceptionResponseDto.to(e.getErrorType().getHttpStatus(), e.getErrorType().getMessage());
 	}
 
 }
