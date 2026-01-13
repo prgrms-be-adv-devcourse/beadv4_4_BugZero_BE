371bb1c4c341956b45be636c8cf7b8b1546f4abe
gumguard <gawonlee0712@gmail.com>
2026-01-13 16:05:52 +0900

    fix(baseIdAndTimeManual): 생성자 추가

--- src/main/java/com/bugzero/rarego/global/jpa/entity/BaseIdAndTimeManual.java
+++ src/main/java/com/bugzero/rarego/global/jpa/entity/BaseIdAndTimeManual.java
@@ -4,11 +4,14 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @MappedSuperclass
 @SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public abstract class BaseIdAndTimeManual extends BaseEntity {
     @Id
