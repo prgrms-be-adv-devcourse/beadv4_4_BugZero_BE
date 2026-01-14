27c7a796d9f4099dd1c9d117afad60ed7c5217e2
pty10510 <pty10510@gmail.com>
2026-01-14 16:39:15 +0900

    feat(product): 상품 정보 등록 api 구

---
+++ src/main/java/com/bugzero/rarego/boundedContext/product/app/ProductFacade.java
@@ -0,0 +1,19 @@
+package com.bugzero.rarego.boundedContext.product.app;
+
+import org.springframework.stereotype.Service;
+
+import com.bugzero.rarego.shared.product.dto.ProductRequestDto;
+import com.bugzero.rarego.shared.product.dto.ProductResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductFacade {
+
+	private final ProductCreateProductUseCase productCreateProductUseCase;
+
+	public ProductResponseDto createProduct(long memberId, ProductRequestDto productRequestDto) {
+		return productCreateProductUseCase.createProduct(memberId, productRequestDto);
+	}
+}
