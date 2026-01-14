1f405a36075d5c75f0ac8ca66bcad14866eb5c29
pty10510 <pty10510@gmail.com>
2026-01-14 16:20:53 +0900

    refactor(product): ProductCondition Enum 속성 추가, ProductImage 에서 isThumbnail 제

--- src/main/java/com/bugzero/rarego/shared/product/dto/ProductAuctionRequestDto.java
+++ src/main/java/com/bugzero/rarego/shared/product/dto/ProductAuctionRequestDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Min;
 public record ProductAuctionRequestDto(
 	@Min(value = 100, message = "입찰 시작가는 최소100원 이상입니다.")
 	int startPrice,
-	@Min(value = 1, message = "걍매기간은 최소 1일이상부터 가능합니다.")
+	@Min(value = 1, message = "경매기간은 최소 1일이상부터 가능합니다.")
 	@Max(value = 30, message = "경매기간은 최대 30일까지만 가능합니다.")
 	int durationDays
 ) {
