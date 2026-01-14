f8c88eadb31fd0fced1f4db02e11baaaaee79eee
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2026-01-14 12:52:55 +0900

    feat(global): Payment 도메인 ErrorType 추가

--- src/main/java/com/bugzero/rarego/global/response/ErrorType.java
+++ src/main/java/com/bugzero/rarego/global/response/ErrorType.java
@@ -6,8 +6,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorType {
-	INTERNAL_SERVER_ERROR(500, "서버 오류가 발생했습니다.")
-	;
+	// server
+	INTERNAL_SERVER_ERROR(500, "서버 오류가 발생했습니다."),
+
+	// member
+	MEMBER_NOT_FOUND(404, "존재하지 않는 회원입니다."),
+
+	// auction
+
+	// product
+
+	// payment
+	WALLET_NOT_FOUND(404, "회원의 지갑이 존재하지 않습니다."),
+	INSUFFICIENT_BALANCE(400, "예치금 잔액이 부족합니다."),
+	INSUFFICIENT_HOLDING(400, "환급할 보증금이 부족합니다.");
 
 	private final Integer httpStatus;
 	private final String message;
