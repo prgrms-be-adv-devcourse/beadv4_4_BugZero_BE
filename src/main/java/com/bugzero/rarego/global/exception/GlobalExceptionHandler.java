a175ccd9846ffab6be4900f8fb64d602cc1c300b
jin214930 <jin214930@naver.com>
2026-01-14 10:59:36 +0900

    feat(global): validation 에러 핸들러 코드 추가

--- src/main/java/com/bugzero/rarego/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bugzero/rarego/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.bugzero.rarego.global.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,5 +17,11 @@ public class GlobalExceptionHandler {
 	public ExceptionResponseDto handleCustomException(CustomException e) {
 		log.error("CustomException 발생: {}", e.getMessage());
 		return ExceptionResponseDto.to(e.getErrorType().getHttpStatus(), e.getErrorType().getMessage());
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ExceptionResponseDto handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		log.error("MethodArgumentNotValidException 발생: {}", e.getMessage());
+		return ExceptionResponseDto.to(HttpStatus.BAD_REQUEST.value(), e.getMessage());
 	}
 }
