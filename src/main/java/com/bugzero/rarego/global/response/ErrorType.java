bbedb90f756262d4d350e18f11b4db1b1c4a38af
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2026-01-14 14:36:58 +0900

    refactor(response): ErrorType에 코드값 포함하도록 변경

--- src/main/java/com/bugzero/rarego/global/response/ErrorType.java
+++ src/main/java/com/bugzero/rarego/global/response/ErrorType.java
@@ -6,21 +6,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorType {
-	// server
-	INTERNAL_SERVER_ERROR(500, "서버 오류가 발생했습니다."),
+	// Global (9000 ~ 9999)
+	INTERNAL_SERVER_ERROR(500, 9000, "서버 오류가 발생했습니다."),
 
-	// member
-	MEMBER_NOT_FOUND(404, "존재하지 않는 회원입니다."),
+	// Member (1000 ~ 1999)
+	MEMBER_NOT_FOUND(404, 1001, "존재하지 않는 회원입니다."),
 
-	// auction
+	// Auction (2000 ~ 2999)
 
-	// product
+	// Product (3000 ~ 3999)
 
-	// payment
-	WALLET_NOT_FOUND(404, "회원의 지갑이 존재하지 않습니다."),
-	INSUFFICIENT_BALANCE(400, "예치금 잔액이 부족합니다."),
-	INSUFFICIENT_HOLDING(400, "환급할 보증금이 부족합니다.");
+	// Payment (4000 ~ 4999)
+	WALLET_NOT_FOUND(404, 4001, "회원의 지갑이 존재하지 않습니다."),
+	INSUFFICIENT_BALANCE(400, 4002, "예치금 잔액이 부족합니다."),
+	INSUFFICIENT_HOLDING(400, 4003, "환급할 보증금이 부족합니다.");
 
 	private final Integer httpStatus;
+	private final int code;
 	private final String message;
 }
