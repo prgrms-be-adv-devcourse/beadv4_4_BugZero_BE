eed5b5bf81d35039af6a695148502730b1ae09b9
pty10510 <pty10510@gmail.com>
2026-01-14 10:35:40 +0900

    refactor(product): ProductRequestDto에서 상품 이미지 복수 등록을 위해 List 타입으로 변경

--- src/main/java/com/bugzero/rarego/boundedContext/product/domain/ProductRequestDto.java
+++ src/main/java/com/bugzero/rarego/boundedContext/product/domain/ProductRequestDto.java
@@ -1,10 +1,12 @@
 package com.bugzero.rarego.boundedContext.product.domain;
 
+import java.util.List;
+
 public record ProductRequestDto(
 	String name,
 	Category category,
 	String description,
 	ProductAuctionRequestDto productAuctionRequestDto,
-	ProductImageRequestDto productImageRequestDto
+	List<ProductImageRequestDto> productImageRequestDto
 ) {
 }
