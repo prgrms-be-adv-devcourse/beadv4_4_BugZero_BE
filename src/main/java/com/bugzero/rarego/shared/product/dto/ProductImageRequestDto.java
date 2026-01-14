094e2c96a63820f316b30833cb7cd6227026a9fa
pty10510 <pty10510@gmail.com>
2026-01-14 14:41:28 +0900

    refactor(product): requestDto 에 Valid 어노테이션을 추가하여 약속된 범위내 값만 들어올 수 있도록 처리

--- src/main/java/com/bugzero/rarego/shared/product/dto/ProductImageRequestDto.java
+++ src/main/java/com/bugzero/rarego/shared/product/dto/ProductImageRequestDto.java
@@ -1,7 +1,22 @@
 package com.bugzero.rarego.shared.product.dto;
 
+import com.bugzero.rarego.boundedContext.product.domain.Product;
+import com.bugzero.rarego.boundedContext.product.domain.ProductImage;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public record ProductImageRequestDto(
-	boolean thumbnail,
+	@NotBlank(message = "이미지 url 정보가 필요합니다.")
+	String imgUrl,
+	@PositiveOrZero(message = "사진 순서는 음수값을 사용하지 못합니다.")
 	int sortOrder
 ) {
+	public ProductImage toEntity(Product product) {
+		return ProductImage.builder()
+			.product(product)
+			.imageUrl(imgUrl)
+			.sortOrder(sortOrder)
+			.build();
+	}
 }
--- src/main/java/com/bugzero/rarego/shared/product/dto/ProductResponseDto.java
+++ src/main/java/com/bugzero/rarego/shared/product/dto/ProductResponseDto.java
@@ -2,6 +2,9 @@ package com.bugzero.rarego.shared.product.dto;
 
 import com.bugzero.rarego.boundedContext.product.domain.InspectionStatus;
 
+import lombok.Builder;
+
+@Builder
 public record ProductResponseDto (
 	long productId,
 	long auctionId,
