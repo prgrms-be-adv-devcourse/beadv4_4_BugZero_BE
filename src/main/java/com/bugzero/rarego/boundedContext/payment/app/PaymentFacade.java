155037caf624d3d74ace00ce5b584112749905f4
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2026-01-14 12:42:52 +0900

    feat(payment): 보증금 홀드 UseCase 구현

--- src/main/java/com/bugzero/rarego/boundedContext/payment/app/PaymentFacade.java
+++ src/main/java/com/bugzero/rarego/boundedContext/payment/app/PaymentFacade.java
@@ -1,4 +1,22 @@
 package com.bugzero.rarego.boundedContext.payment.app;
 
+import com.bugzero.rarego.shared.payment.dto.DepositHoldRequest;
+import com.bugzero.rarego.shared.payment.dto.DepositHoldResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class PaymentFacade {
+
+	private final PaymentHoldDepositUseCase paymentHoldDepositUseCase;
+
+	/**
+	 * 보증금 홀딩
+	 */
+	@Transactional
+	public DepositHoldResponse holdDeposit(DepositHoldRequest request) {
+		return paymentHoldDepositUseCase.holdDeposit(request);
+	}
 }
