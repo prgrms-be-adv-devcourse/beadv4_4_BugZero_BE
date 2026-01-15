56644137d7cd9f14d17c5c97f87747b17dfeb843
gumguard <gawonlee0712@gmail.com>
2026-01-15 14:31:30 +0900

    feat(auth): 인증 테이블 Account 구현

---
+++ src/main/java/com/bugzero/rarego/boundedContext/auth/domain/Account.java
@@ -0,0 +1,44 @@
+package com.bugzero.rarego.boundedContext.auth.domain;
+
+import com.bugzero.rarego.global.jpa.entity.BaseIdAndTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+	name = "AUTH_ACCOUNT",
+	uniqueConstraints = {
+		@UniqueConstraint(
+			name = "uk_auth_account_provider_provider_id",
+			columnNames = {"provider", "provider_id"}
+		)
+	}
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Account extends BaseIdAndTime{
+
+	// TODO: member와 연관관계 publicId로 변경
+	@Column(name = "member_id", nullable = false)
+	private Long memberId;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "provider", nullable = false)
+	private Provider provider;
+
+	@Column(name = "provider_id", length = 128, nullable = false)
+	private String providerId;
+}
---
+++ src/main/java/com/bugzero/rarego/boundedContext/auth/domain/Provider.java
@@ -0,0 +1,5 @@
+package com.bugzero.rarego.boundedContext.auth.domain;
+
+public enum Provider {
+	GOOGLE, KAKAO, NAVER
+}
