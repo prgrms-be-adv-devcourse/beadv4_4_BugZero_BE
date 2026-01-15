ca5aefc92e62ffcc19d585bdad2e924c3a08cb15
gumguard <gawonlee0712@gmail.com>
2026-01-15 15:26:04 +0900

    fix(auth): 테스트 컨트롤러 수정

--- src/main/java/com/bugzero/rarego/boundedContext/auth/domain/TokenIssueDto.java
+++ src/main/java/com/bugzero/rarego/boundedContext/auth/domain/TokenIssueDto.java
@@ -1,4 +1,10 @@
 package com.bugzero.rarego.boundedContext.auth.domain;
 
-public record TokenIssueDto(Long memberId, String role) {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record TokenIssueDto(
+	@NotNull @Positive Long memberId,
+	@NotBlank String role
+) { }
