12b949bb5a250e88c1a68b05fd7e3b06dfed2871
gumguard <gawonlee0712@gmail.com>
2026-01-15 11:38:47 +0900

    test(auth): authServiceTest 주입 오류 수정

--- src/test/java/com/bugzero/rarego/boundedContext/auth/app/AuthServiceTest.java
+++ src/test/java/com/bugzero/rarego/boundedContext/auth/app/AuthServiceTest.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @Transactional
 public class AuthServiceTest {
 	@Autowired
