f0967815fd0da04abad2ea6fee7c3ab04d45e11c
pty10510 <pty10510@gmail.com>
2026-01-16 14:41:52 +0900

    feat(product): 상품 등록 로직 내부에 경매 정보 생성 API 구현

--- src/main/java/com/bugzero/rarego/boundedContext/product/app/ProductCreateProductUseCase.java
+++ src/main/java/com/bugzero/rarego/boundedContext/product/app/ProductCreateProductUseCase.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.bugzero.rarego.boundedContext.product.domain.Product;
 import com.bugzero.rarego.boundedContext.product.out.ProductRepository;
+import com.bugzero.rarego.shared.product.auction.out.AuctionApiClient;
 import com.bugzero.rarego.shared.product.dto.ProductRequestDto;
 import com.bugzero.rarego.shared.product.dto.ProductResponseDto;
 
@@ -14,11 +15,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductCreateProductUseCase {
 	private final ProductRepository productRepository;
+	private final AuctionApiClient auctionApiClient;
 
 	@Transactional
-	public ProductResponseDto createProduct(long memberId, ProductRequestDto productRequestDto) {
+	public ProductResponseDto createProduct(String memberUUID, ProductRequestDto productRequestDto) {
 
-		//TODO 멤버id가 아닌 멤버를 직접받을 수 있도록 변경
+		//TODO UUID를 통해 멤버 pk ID 값을 구하여 전달
+		Long memberId = 1L;
 		Product product = productRequestDto.toEntity(memberId);
 
 		//상품 이미지 정보 저장
@@ -29,13 +32,15 @@ public class ProductCreateProductUseCase {
 		// 부모만 저장 (CascadeType.PERSIST에 의해 자식인 ProductImage들도 자동으로 INSERT됨)
 		Product savedProduct = productRepository.save(product);
 
-		//TODO 경매정보 생성 요청하는 api
+		//경매정보 생성 요청하는 api
+		long auctionId = auctionApiClient.createAuction(savedProduct.getId(), memberUUID,
+			productRequestDto.productAuctionRequestDto());
 
 		//TODO 검수 요청하는 api or event
 
 		return ProductResponseDto.builder()
 			.productId(savedProduct.getId())
-			.auctionId(1)
+			.auctionId(auctionId)
 			.inspectionStatus(savedProduct.getInspectionStatus())
 			.build();
 	}
