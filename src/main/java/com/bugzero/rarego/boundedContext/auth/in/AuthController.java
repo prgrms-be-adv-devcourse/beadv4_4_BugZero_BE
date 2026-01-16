618e7d4232d6841ca0a4f1805fc2916284b0473b
gumguard <gawonlee0712@gmail.com>
2026-01-16 11:44:48 +0900

    fix(auth): publicId 명칭, parser 변경

--- src/main/java/com/bugzero/rarego/boundedContext/auth/in/AuthController.java
+++ src/main/java/com/bugzero/rarego/boundedContext/auth/in/AuthController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 import com.bugzero.rarego.boundedContext.auth.app.AuthService;
 import com.bugzero.rarego.boundedContext.auth.domain.TokenIssueDto;
 import com.bugzero.rarego.global.response.SuccessResponseDto;
