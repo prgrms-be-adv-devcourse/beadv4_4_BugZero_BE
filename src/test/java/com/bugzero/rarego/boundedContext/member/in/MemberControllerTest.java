e934e7c436c6220864da66f7ca9f185689cc3bf1
geolyun <geolyun@gmail.com>
2026-01-16 17:12:17 +0900

    feat(auction): 나의 판매 목록 조회 API 구현

--- src/test/java/com/bugzero/rarego/boundedContext/member/in/MemberControllerTest.java
+++ src/test/java/com/bugzero/rarego/boundedContext/member/in/MemberControllerTest.java
@@ -9,17 +9,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.MethodParameter;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
 
 import com.bugzero.rarego.boundedContext.auction.app.AuctionFacade;
 import com.bugzero.rarego.boundedContext.auction.domain.AuctionOrderStatus;
@@ -27,6 +36,7 @@ import com.bugzero.rarego.boundedContext.auction.domain.AuctionStatus;
 import com.bugzero.rarego.global.aspect.ResponseAspect;
 import com.bugzero.rarego.global.response.PageDto;
 import com.bugzero.rarego.global.response.PagedResponseDto;
+import com.bugzero.rarego.global.security.MemberPrincipal;
 import com.bugzero.rarego.shared.auction.dto.AuctionFilterType;
 import com.bugzero.rarego.shared.auction.dto.MyBidResponseDto;
 import com.bugzero.rarego.shared.auction.dto.MySaleResponseDto;
@@ -36,15 +46,55 @@ import com.bugzero.rarego.shared.auction.dto.MySaleResponseDto;
 @EnableAspectJAutoProxy
 class MemberControllerTest {
 
-	@Autowired
 	private MockMvc mockMvc;
+
+	@Autowired
+	private MemberController memberController;
 
 	@MockitoBean
 	private AuctionFacade auctionFacade;
 
+	@BeforeEach
+	void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(memberController)
+			.setCustomArgumentResolvers(
+				// Pageable resolver
+				new PageableHandlerMethodArgumentResolver(),
+				// UserDetails resolver (for getMyBids)
+				new HandlerMethodArgumentResolver() {
+					@Override
+					public boolean supportsParameter(MethodParameter parameter) {
+						return UserDetails.class.isAssignableFrom(parameter.getParameterType());
+					}
+
+					@Override
+					public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+						NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+						return User.withUsername("2")
+							.password("password")
+							.roles("USER")
+							.build();
+					}
+				},
+				// MemberPrincipal resolver (for getMySales)
+				new HandlerMethodArgumentResolver() {
+					@Override
+					public boolean supportsParameter(MethodParameter parameter) {
+						return MemberPrincipal.class.isAssignableFrom(parameter.getParameterType());
+					}
+
+					@Override
+					public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+						NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+						return new MemberPrincipal("2", "SELLER");
+					}
+				}
+			)
+			.build();
+	}
+
 	@Test
 	@DisplayName("GET /members/me/bids - 내 입찰 내역 조회 성공")
-	@WithMockUser(username = "2")
 	void getMyBids_success() throws Exception {
 		// given
 		Long memberId = 2L;
@@ -69,9 +119,9 @@ class MemberControllerTest {
 			.andExpect(jsonPath("$.data[0].auctionStatus").value("IN_PROGRESS"));
 	}
 
+
 	@Test
 	@DisplayName("GET /members/me/sales - 내 판매 목록 조회 성공")
-	@WithMockUser(username = "2")
 	void getMySales_success() throws Exception {
 		// given
 		Long memberId = 2L;
