f4047e62c70cf4e7b8b7d8b56a3e9c04838b8b82
pty10510 <pty10510@gmail.com>
2026-01-16 14:33:27 +0900

    feat(product): 상품 등록 시 경매 정보 생성 요청 내부 api 구

---
+++ src/main/java/com/bugzero/rarego/boundedContext/product/auction/in/InternalAuctionController.java
@@ -0,0 +1,33 @@
+package com.bugzero.rarego.boundedContext.product.auction.in;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bugzero.rarego.boundedContext.product.auction.app.AuctionCreateAuctionUseCase;
+import com.bugzero.rarego.global.response.SuccessResponseDto;
+import com.bugzero.rarego.global.response.SuccessType;
+import com.bugzero.rarego.shared.product.dto.ProductAuctionRequestDto;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auctions")
+@RequiredArgsConstructor
+public class InternalAuctionController {
+
+	private final AuctionCreateAuctionUseCase auctionCreateAuctionUseCase;
+
+	@PostMapping("/{productId}/auctions/{sellerUUID}")
+	public SuccessResponseDto<Long> createAuction (
+		@PathVariable Long productId,
+		@PathVariable String sellerUUID,
+		@Valid @RequestBody ProductAuctionRequestDto productAuctionRequestDto
+	) {
+		return SuccessResponseDto.from(SuccessType.CREATED,
+			auctionCreateAuctionUseCase.createAuction(productId, sellerUUID, productAuctionRequestDto));
+	}
+}
