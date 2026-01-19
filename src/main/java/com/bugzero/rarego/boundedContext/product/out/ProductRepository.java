13a02c272773b4a1d0838e27ff456eb4a0e8d404
pty10510 <pty10510@gmail.com>
2026-01-19 16:24:21 +0900

    feat(product): 유효한 상품인지 확인하는 jpa 메서드 추가

--- src/main/java/com/bugzero/rarego/boundedContext/product/out/ProductRepository.java
+++ src/main/java/com/bugzero/rarego/boundedContext/product/out/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.bugzero.rarego.boundedContext.product.out;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bugzero.rarego.boundedContext.product.domain.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	Optional<Product> findByIdAndDeletedIsFalse(Long id);
 }
