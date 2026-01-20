172f69a0e57da6d3b07bddd203ca39b9b93bdad9
[Kkimdoyeon] <ehdus5176@gmail.com>
2026-01-20 18:00:47 +0900

    feat(auction): AuctionMemberController로 코드 이전

--- src/main/java/com/bugzero/rarego/boundedContext/member/in/MemberController.java
+++ src/main/java/com/bugzero/rarego/boundedContext/member/in/MemberController.java
@@ -1,20 +1,16 @@
 package com.bugzero.rarego.boundedContext.member.in;
 
 import com.bugzero.rarego.boundedContext.auction.app.AuctionFacade;
-import com.bugzero.rarego.boundedContext.auction.in.dto.WishlistListResponseDto;
 import com.bugzero.rarego.boundedContext.member.app.MemberFacade;
 import com.bugzero.rarego.boundedContext.member.domain.MemberMeResponseDto;
-import com.bugzero.rarego.global.response.PagedResponseDto;
 import com.bugzero.rarego.global.response.SuccessResponseDto;
 import com.bugzero.rarego.global.response.SuccessType;
 import com.bugzero.rarego.global.security.MemberPrincipal;
 import com.bugzero.rarego.shared.member.domain.MemberJoinRequestDto;
 import com.bugzero.rarego.shared.member.domain.MemberJoinResponseDto;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,15 +35,5 @@ public class MemberController {
     public SuccessResponseDto<MemberMeResponseDto> getMe(@AuthenticationPrincipal MemberPrincipal memberPrincipal) {
         MemberMeResponseDto responseDto = memberFacade.getMe(memberPrincipal.publicId(), memberPrincipal.role());
         return SuccessResponseDto.from(SuccessType.OK, responseDto);
-    }
-
-    @SecurityRequirement(name = "bearerAuth")
-    @Operation(summary = "내 관심 경매 목록 조회", description = "내가 찜한 경매 목록을 조회합니다")
-    @GetMapping("/me/bookmarks")
-    public PagedResponseDto<WishlistListResponseDto> getMyBookmarks(
-            @AuthenticationPrincipal MemberPrincipal memberPrincipal,
-            Pageable pageable
-    ) {
-        return auctionFacade.getMyBookmarks(memberPrincipal.publicId(), pageable);
     }
 }
