0a128f0715c786d4809dbe8fbf4da47fffb5bcc0
[Kkimdoyeon] <ehdus5176@gmail.com>
2026-01-20 17:53:03 +0900

    feat(auction): 내 관심 경매 목록 조회 API 구현

--- src/main/java/com/bugzero/rarego/boundedContext/auction/out/AuctionBookmarkRepository.java
+++ src/main/java/com/bugzero/rarego/boundedContext/auction/out/AuctionBookmarkRepository.java
@@ -1,6 +1,8 @@
 package com.bugzero.rarego.boundedContext.auction.out;
 
 import com.bugzero.rarego.boundedContext.auction.domain.AuctionBookmark;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +11,6 @@ public interface AuctionBookmarkRepository extends JpaRepository<AuctionBookmark
     boolean existsByAuctionIdAndMemberId(Long auctionId, Long memberId);
 
     Optional<AuctionBookmark> findByAuctionIdAndMemberId(Long auctionId, Long memberId);
+
+    Page<AuctionBookmark> findAllByMemberId(Long memberId, Pageable pageable);
 }
