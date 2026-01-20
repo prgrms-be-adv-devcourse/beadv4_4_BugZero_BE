fb1a916de070506563d87e4269a6bd9df869a0a6
[Kkimdoyeon] <ehdus5176@gmail.com>
2026-01-20 12:31:19 +0900

    refactor(auction): 예외처리 추가

--- src/main/java/com/bugzero/rarego/boundedContext/auction/app/AuctionBookmarkUseCase.java
+++ src/main/java/com/bugzero/rarego/boundedContext/auction/app/AuctionBookmarkUseCase.java
@@ -4,6 +4,8 @@ import com.bugzero.rarego.boundedContext.auction.domain.Auction;
 import com.bugzero.rarego.boundedContext.auction.domain.AuctionBookmark;
 import com.bugzero.rarego.boundedContext.auction.in.dto.WishlistAddResponseDto;
 import com.bugzero.rarego.boundedContext.auction.out.AuctionBookmarkRepository;
+import com.bugzero.rarego.global.exception.CustomException;
+import com.bugzero.rarego.global.response.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,9 +22,8 @@ public class AuctionBookmarkUseCase {
         Auction auction = auctionSupport.findAuctionById(auctionId);
 
         // 2. 중복 확인
-        if (auctionBookmarkRepository.existsByAuctionIdAndMemberId(auctionId, memberId)) {
-            return WishlistAddResponseDto.of(false, auctionId);
-        }
+        if (auctionBookmarkRepository.existsByAuctionIdAndMemberId(auctionId, memberId))
+            throw new CustomException(ErrorType.BOOKMARK_ALREADY_EXISTS);
 
         // 3. 북마크 저장
         AuctionBookmark bookmark = AuctionBookmark.builder()
