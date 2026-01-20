437259e29c0afb29b50501a1d8e95ceaa861e473
pty10510 <pty10510@gmail.com>
2026-01-20 23:23:21 +0900

    feat(auction):경메 기본정보 수정 내부 api 구현

--- src/main/java/com/bugzero/rarego/boundedContext/product/auction/app/AuctionCreateAuctionUseCase.java
+++ src/main/java/com/bugzero/rarego/boundedContext/product/auction/app/AuctionCreateAuctionUseCase.java
@@ -3,6 +3,7 @@ package com.bugzero.rarego.boundedContext.product.auction.app;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bugzero.rarego.boundedContext.auction.domain.AuctionMember;
 import com.bugzero.rarego.boundedContext.auction.out.AuctionRepository;
 import com.bugzero.rarego.shared.product.dto.ProductAuctionRequestDto;
 
@@ -12,34 +13,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuctionCreateAuctionUseCase {
 	private final AuctionRepository auctionRepository;
+	private final ProductAuctionSupport productAuctionSupport;
 
 	@Transactional
 	// 신규상품 경매 정보 생성
-	public long createAuction(Long productId,String sellerUUID, ProductAuctionRequestDto productAuctionRequestDto) {
-		//TODO sellerUUID를 통해 memberId를 반환하는 메서드 추가 (support에 생성)
-		Long sellerId = 1L;
+	public long createAuction(Long productId,String publicId, ProductAuctionRequestDto productAuctionRequestDto) {
+		AuctionMember seller = productAuctionSupport.getAuctionMember(publicId);
 
-		int tickSize = determineTickSize(productAuctionRequestDto.startPrice());
+		int tickSize = productAuctionSupport.determineTickSize(productAuctionRequestDto.startPrice());
 
 		return auctionRepository
-			.save(productAuctionRequestDto.toEntity(productId, sellerId, tickSize))
+			.save(productAuctionRequestDto.toEntity(productId, seller.getId(), tickSize))
 			.getId();
 	}
 
-	// 시작가에 따라 호가단위 결정
-	private int determineTickSize(int startPrice) {
-		if (startPrice < 10000) {
-			return 500;
-		} else if (startPrice < 50000) {
-			return 1000;
-		} else if (startPrice < 100000) {
-			return 2000;
-		} else if (startPrice < 300000) {
-			return 5000;
-		} else if (startPrice < 1000000) {
-			return 10000;
-		} else {
-			return 30000; // 100만 원 이상
-		}
-	}
+
 }
