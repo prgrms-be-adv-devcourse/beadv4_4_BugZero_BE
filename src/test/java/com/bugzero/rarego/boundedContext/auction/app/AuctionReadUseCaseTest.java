45ea6a00e8177616fdeb3ffdc6859f1eaedff697
sdg_0__0 <geolyun@gmail.com>
2026-01-21 12:11:58 +0900

    chore(auction): import 추가
    chore(auction): import 추가

--- src/test/java/com/bugzero/rarego/boundedContext/auction/app/AuctionReadUseCaseTest.java
+++ src/test/java/com/bugzero/rarego/boundedContext/auction/app/AuctionReadUseCaseTest.java
@@ -45,6 +45,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import org.mockito.ArgumentCaptor;
+import static org.mockito.Mockito.*;
 
 import jakarta.persistence.criteria.*;
 
