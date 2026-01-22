f7b275c16a45a32b0740b302e63580c149819c22
gumguard <gawonlee0712@gmail.com>
2026-01-22 16:43:07 +0900

    feat(auth): 탈퇴 요건 주석 수정

--- src/main/java/com/bugzero/rarego/boundedContext/auth/app/AuthWithdrawAccountUseCase.java
+++ src/main/java/com/bugzero/rarego/boundedContext/auth/app/AuthWithdrawAccountUseCase.java
@@ -28,18 +28,18 @@ public class AuthWithdrawAccountUseCase {
 	public void withdraw(String accessToken) {
 		String publicId = extractPublicId(accessToken);
 
-		Account account = authSupport.findByMemberPublicId(publicId);
+		Account account = authSupport.findByPublicId(publicId);
+		// 삭제된 계정 404
 		if (account.isDeleted()) {
-			throw new CustomException(ErrorType.AUTH_ACCOUNT_DELETED);
+			throw new CustomException(ErrorType.AUTH_ACCOUNT_NOT_FOUND);
 		}
 
 		/**
 		 * TODO:
 		 * 진행중인 product, bid, payment가 있는지 확인
-		 *     1. 내 상품 조회 /members/me/products
-		 *     2. 내 입찰 조회 /members/me/bids
-		 *     3. 내 판매 물품 조회 /members/me/sales
-		 *     4. 내 낙찰 주문 조회 /members/me/orders
+		 *     1. 내 입찰 조회 /members/me/bids => 내가 입찰한 BID가 있는 Auction이 모두 ENDED
+		 *     2. 내 판매 물품 조회 /members/me/sales => 내가 검수 맡기고, 경매한 product 모두 완료되어 Auction ENDED
+		 *     3. 내 낙찰 주문 조회 /members/me/orders => 내가 낙찰받는 주문이 AuctionOrderStatus.PROCESSING이 아니어야함
 		 */
 
 		memberApiClient.withdraw(publicId);
