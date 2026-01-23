55e0a00d96dfded693e8843ec726ed025b2d1c4a
jin214930 <jin214930@naver.com>
2026-01-23 12:31:00 +0900

    test(payment): 테스트 코드 작성

--- src/main/java/com/bugzero/rarego/boundedContext/payment/app/PaymentGetMyWalletUseCase.java
+++ src/main/java/com/bugzero/rarego/boundedContext/payment/app/PaymentGetMyWalletUseCase.java
@@ -1,6 +1,7 @@
 package com.bugzero.rarego.boundedContext.payment.app;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bugzero.rarego.boundedContext.payment.domain.PaymentMember;
 import com.bugzero.rarego.boundedContext.payment.in.dto.WalletResponseDto;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 public class PaymentGetMyWalletUseCase {
 	private final PaymentSupport paymentSupport;
 
+	@Transactional(readOnly = true)
 	public WalletResponseDto getMyWallet(String memberPublicId) {
 		PaymentMember member = paymentSupport.findMemberByPublicId(memberPublicId);
 		return WalletResponseDto.from(paymentSupport.findWalletByMember(member));
